52b1766cd081fee3715738f991d2bcec35f06572
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-03-10 17:26:27 +0100

    Starting Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -43,28 +43,28 @@ class Game extends React.Component {
         return (
             <div>
                 <div className="line" >
-                    <div className="card" onClick={(e) => this.ImageAnime(e)}><img src={this.state.picture[0]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(1)}><img className={this.state.image1? "animIn" : ""} src={this.state.picture[0]}/></div>
                     <div className="card" onClick={() => this.ImageAnime(2)}><img className={this.state.image2? "animIn" : ""} src={this.state.picture[1]}/></div>
-                    <div className="card" onClick={() => this.ImageAnime(3)}><img src={this.state.picture[2]}/></div>
-                    <div className="card" onClick={() => this.ImageAnime(4)}><img src={this.state.picture[3]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(3)}><img className={this.state.image3? "animIn" : ""} src={this.state.picture[2]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(4)}><img className={this.state.image4? "animIn" : ""} src={this.state.picture[3]}/></div>
                 </div>
                 <div className="line">
-                    <div className="card" onClick={() => this.ImageAnime(5)}><img src={this.state.picture[4]}/></div>
-                    <div className="card" onClick={() => this.ImageAnime(6)}><img src={this.state.picture[5]}/></div>
-                    <div className="card" onClick={() => this.ImageAnime(7)}><img src={this.state.picture[6]}/></div>
-                    <div className="card" onClick={() => this.ImageAnime(8)}><img src={this.state.picture[7]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(5)}><img className={this.state.image5? "animIn" : ""} src={this.state.picture[4]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(6)}><img className={this.state.image6? "animIn" : ""} src={this.state.picture[5]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(7)}><img className={this.state.image7? "animIn" : ""} src={this.state.picture[6]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(8)}><img className={this.state.image8? "animIn" : ""} src={this.state.picture[7]}/></div>
                 </div>
                 <div className="line">
-                    <div className="card" onClick={() => this.ImageAnime(9)}><img src={this.state.picture[0]}/></div>
-                    <div className="card" onClick={() => this.ImageAnime(10)}><img src={this.state.picture[7]}/></div>
-                    <div className="card" onClick={() => this.ImageAnime(11)}><img src={this.state.picture[1]}/></div>
-                    <div className="card" onClick={() => this.ImageAnime(12)}><img src={this.state.picture[2]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(9)}><img className={this.state.image9? "animIn" : ""} src={this.state.picture[0]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(10)}><img className={this.state.image10? "animIn" : ""} src={this.state.picture[7]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(11)}><img className={this.state.image11? "animIn" : ""} src={this.state.picture[1]}/></div>
+                    <div className="card" onClick={() => this.ImageAnime(12)}><img className={this.state.image12? "animIn" : ""} src={this.state.picture[2]}/></div>
                 </div>
                 <div className="line">
-                    <div className="card" onClick={(e) => this.ImageAnime(13)}><img src={this.state.picture[3]}/></div>
-                    <div className="card" onClick={(e) => this.ImageAnime(14)}><img src={this.state.picture[4]}/></div>
-                    <div className="card" onClick={(e) => this.ImageAnime(15)}><img src={this.state.picture[5]}/></div>
-                    <div className="card" onClick={(e) => this.ImageAnime(16)}><img src={this.state.picture[6]}/></div>
+                    <div className="card" onClick={(e) => this.ImageAnime(13)}><img className={this.state.image13? "animIn" : ""} src={this.state.picture[3]}/></div>
+                    <div className="card" onClick={(e) => this.ImageAnime(14)}><img className={this.state.image14? "animIn" : ""} src={this.state.picture[4]}/></div>
+                    <div className="card" onClick={(e) => this.ImageAnime(15)}><img className={this.state.image15? "animIn" : ""} src={this.state.picture[5]}/></div>
+                    <div className="card" onClick={(e) => this.ImageAnime(16)}><img className={this.state.image16? "animIn" : ""} src={this.state.picture[6]}/></div>
                 </div>
             </div>
         );
