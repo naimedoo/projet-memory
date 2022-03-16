cabd91fabf92aa6a61902fc7d6e3617284e1f14c
SoraNoTami <92116172+SoraNoTami@users.noreply.github.com>
2022-03-16 11:55:47 +0100

    Game is better

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -11,10 +11,12 @@ class Score extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Score</h1>
-        <h3>Player 1: <b>{this.props.score1}</b></h3>
-        <h4>Turn: {this.props.turn}</h4>
+      <div className='end'>
+        <div className='scoreCard'>
+          <h1>Score</h1>
+          <h3>Player 1: <b>{this.props.score1}</b></h3>
+          <h4>Turn: {this.props.turn}</h4>
+        </div>
       </div>
     );
   }
